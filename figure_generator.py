d0a291a4c56b0b303c0d61afae370a9d7f3d9432
Wang <mwang106@ur.rochester.edu>
2023-12-10 19:56:29 -0500

    geo figures

--- figure_generator.py
+++ figure_generator.py
@@ -7,10 +7,15 @@ from utils import load_data, CANDIDATES
 
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 from matplotlib.dates import DateFormatter, DayLocator
 import seaborn as sns
 
+
+def fmt(string: str) -> str:
+    return string.replace("_", " ").title()
 
 def save_stackplot(name: str, target: str, ylabel: str, x, **kwargs) -> None:
     """
@@ -23,8 +28,6 @@ def save_stackplot(name: str, target: str, ylabel: str, x, **kwargs) -> None:
     - `x`: x axis, usually time
     - `kwargs`: key-value pairs of column name and its value, usually the value is a list of y axis
     """
-    os.makedirs(f"figures/{ylabel}/{name}", exist_ok=True)
-
     fig, ax = plt.subplots(figsize=(9, 6), dpi=300)
     
     # declare type, this helps intellicode and pylint
@@ -55,11 +58,11 @@ def save_stackplot(name: str, target: str, ylabel: str, x, **kwargs) -> None:
         ax.set_ylabel(ylabel)
         ax.set_ylim(0)
     ax.set_title(
-        f'{target.replace("_", " ").title()} Stackplot'
-        f' with #{name.title()} Tweets')
+        f'{fmt(target)} Stackplot with #{name.title()} Tweets')
 
     fig.legend(loc='outside lower center', ncols=min(10, len(kwargs)))
 
+    os.makedirs(f"figures/{ylabel}/{name}", exist_ok=True)
     fig.savefig(f"figures/{ylabel}/{name}/{target}.png")
     fig.clear()
 
@@ -98,10 +101,98 @@ def plot_candidate_multiclass(candidate_name: str, target_prefix: str = 'stance_
                    })
 
 
+def cal_score(df: pd.DataFrame, prefix: str, score_name: str) -> pd.DataFrame:
+    # using sentiment because its easy, only to compute no. of tweets
+    cols = [f'sentiment_{s}' for s in ['negative', 'positive', 'neutral']]
+    s = 'No. of Tweets'
+    df[s] = df[cols].sum(axis=1)
+    
+    neg = f'{prefix}_negative'
+    pos = f'{prefix}_positive'
+    assert neg in df.columns and pos in df.columns, f'no neg and pos cols'
+    df[score_name] = df.apply(lambda x: (x[pos] - x[neg]) / x[s], axis=1)
+    # df[score_name] = (df[pos]-df[neg]) / df[s]
+    # print(df[pos].value_counts().sort_index())
+    # exit(0)
+    return df
+    
+
+def plot_candidate_geo(candidate_name: str, target_prefix: str = 'stance_biden'):
+    # download from US Census
+    states = [ 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
+           'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
+           'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
+           'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
+           'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
+    usa = gpd.read_file('shp/cb_2022_us_state_5m.shp')
+    usa = usa[usa['STUSPS'].isin(states)]
+    
+    par_data = load_data(candidate_name)
+    # get all columns with target_prefix
+    target_col = sorted(list(filter(
+        lambda x: x.startswith(target_prefix),
+        par_data.columns
+    )))
+    assert target_col, f'no column found with prefix `{target_prefix}`'
+    assert len(target_col) == 3, f'column is not neg-neu-pos form {target_col}'
+    
+    # drop tweets that has no geo location info
+    par_data = par_data.dropna(subset=['lat', 'long'])
+    
+    # filter lat and long to get only us mainland tweets
+    lat = par_data['lat']
+    long = par_data['long']
+    par_data = par_data[
+        (lat > 19) & (lat < 50) & 
+        (long > -128) & (long < -50) & 
+        (par_data['country'] == 'United States of America')
+    ]
+    score_name = f'{fmt(target_prefix)} Score'
+    
+    # draw graph according to lat and long onto a map
+    scatter_data = par_data.groupby(['lat', 'long']).sum(numeric_only=True)
+    scatter_data = cal_score(scatter_data, target_prefix, score_name)
+    fig, ax = plt.subplots(figsize=(16, 9), dpi=300)
+    usa.boundary.plot(ax=ax, linewidth=1, alpha=0.1, color='grey')
+    # sns.relplot(data=scatter_data, x="total_bill", y="tip", col="time", hue="day", style="day", kind="scatter")
+    sns.scatterplot(scatter_data, 
+                    x='long', y='lat', alpha=0.1, 
+                    hue=score_name, palette='Spectral',
+                    size='No. of Tweets', sizes=(50, 200),
+                    ax=ax, legend=True)
+    ax.set_title(f'{fmt(target_prefix)} Score HeatMap with #{candidate_name.title()} Tweets')
+    os.makedirs(f"figures/national/{candidate_name}", exist_ok=True)
+    fig.savefig(f"figures/national/{candidate_name}/{target_prefix}.png")
+    
+    # draw graph for each state, categorized, then display on map
+    state_data = par_data.groupby(['state_code']).sum(numeric_only=True)
+    state_data = cal_score(state_data, target_prefix, score_name)
+    state_data = usa.merge(state_data, left_on='STUSPS', right_index=True, how='left')
+    
+    cmap = sns.color_palette('Spectral', as_cmap=True)
+    norm = mcolors.Normalize(vmin=-1, vmax=1, clip=True)
+    mapper = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax: Axes = ax
+    ax.set_title(f'{fmt(target_prefix)} Score State HeatMap with #{candidate_name.title()} Tweets')
+    state_data.plot(ax=ax, linewidth=1, alpha=0.1)
+
+    for row in state_data.itertuples():
+        vf = state_data[state_data.STUSPS==row.STUSPS]
+        c = mcolors.to_hex(mapper.to_rgba(vf[score_name]))
+        vf.plot(color=c, linewidth=0.8, ax=ax, edgecolor='0.9')
+
+    fig.colorbar(mapper, ax=ax)
+    os.makedirs(f"figures/states/{candidate_name}", exist_ok=True)
+    fig.savefig(f"figures/states/{candidate_name}/{target_prefix}.png")
+
+
 if __name__ == '__main__':
-    target_cols = ['emotion', 'language',
+    targets = ['emotion', 'language',
                    'sentiment', 'stance_biden', 'stance_trump']
+    targets = ['sentiment']
     # target_cols = ['language']
-    for col in target_cols:
+    for col in targets:
         for par_name in CANDIDATES:
-            plot_candidate_multiclass(par_name, col)
+            plot_candidate_geo(par_name, col)
